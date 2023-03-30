1c869f8294b88d9f842f30ce632e3381ee68c6fd
Oran RH <oranroha@gmail.com>
2023-03-30 12:19:12 -0400

    better

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ const c2 = document.getElementById("canvas");
 const ctx2 = c2.getContext("2d");
 
 const DEBUG = false;
+const HIGH_RATE = true;
 if (DEBUG) (canvas = c2), (ctx = ctx2);
 
 canvas.width = window.innerWidth;
@@ -33,48 +34,50 @@ async function startup() {
       facingMode: facingMode
     }
   };
-  let zeroArea = 200000;
+  let zeroArea = 50000;
   video.srcObject = await navigator.mediaDevices.getUserMedia(constraints);
-  setInterval(async () => {
-    ctx.drawImage(video, 0, 0, canvas.width, canvas.height);
-    let url = canvas.toDataURL("image/jpeg");
-    let img = document.createElement("img");
-    img.src = url;
-    img.onload = async () => {
-      const detection = await faceapi.detectAllFaces(
-        img,
-        new faceapi.SsdMobilenetv1Options({ minConfidence: 0.5 })
-      );
-      if (!DEBUG) {
-        ctx2.fillStyle = "black";
-        ctx2.fillRect(0, 0, c2.width, c2.height);
-        ctx2.fillStyle = "#003300";
-        ctx2.beginPath();
-        ctx2.moveTo(c2.width * 0.4, 0);
-        ctx2.lineTo(c2.width * 0.6, 0);
-        ctx2.lineTo(c2.width, c2.height);
-        ctx2.lineTo(0, c2.height);
-        ctx2.lineTo(c2.width * 0.4, 0);
-        ctx2.closePath();
-        ctx2.fill();
-      }
-      document.addEventListener("keydown", (e) => {
-        if (e.key === "ArrowUp") zeroArea += 50;
-        else if (e.key === "ArrowDown") zeroArea -= 50;
-        if (zeroArea < 0) zeroArea = 0;
-      });
-      detection.forEach((face) => {
-        let dist = zeroArea / (face.box.width * face.box.height);
-        let posY = dist * (c2.height / 2);
-        let posX = face.box.x + face.box.width / 2 - canvas.width / 2;
-        posX *= (posY / canvas.height) * 0.8 + 0.2;
-        posX += canvas.width / 2;
-        let img = document.createElement("img");
-        img.src = "/pride.png";
-        img.onload = () => {
-          ctx2.drawImage(img, c2.width - posX - 50, posY - 25, 100, 50);
-          ctx2.strokeStyle = "white";
-          ctx2.strokeWidth = 20;
+  setInterval(
+    async () => {
+      ctx.drawImage(video, 0, 0, canvas.width, canvas.height);
+      let url = canvas.toDataURL("image/jpeg");
+      let img = document.createElement("img");
+      img.src = url;
+      img.onload = async () => {
+        const detection = await faceapi.detectAllFaces(
+          img,
+          new faceapi.SsdMobilenetv1Options({
+            minConfidence: HIGH_RATE ? 0.1 : 0.5
+          })
+        );
+        if (!DEBUG) {
+          ctx2.fillStyle = "black";
+          ctx2.fillRect(0, 0, c2.width, c2.height);
+          ctx2.fillStyle = "#003300";
+          ctx2.beginPath();
+          ctx2.moveTo(c2.width * 0.4, 0);
+          ctx2.lineTo(c2.width * 0.6, 0);
+          ctx2.lineTo(c2.width, c2.height);
+          ctx2.lineTo(0, c2.height);
+          ctx2.lineTo(c2.width * 0.4, 0);
+          ctx2.closePath();
+          ctx2.fill();
+        }
+        document.addEventListener("keydown", (e) => {
+          if (e.key === "ArrowUp")
+            zeroArea += 20 * (1 / (1 + zeroArea / 100000));
+          else if (e.key === "ArrowDown")
+            zeroArea -= 20 * (1 / (1 + zeroArea / 100000));
+          if (zeroArea < 0) zeroArea = 0;
+        });
+        detection.forEach((face) => {
+          let dist = zeroArea / (face.box.width * face.box.height);
+          let posY = dist * (c2.height / 2);
+          let posX = face.box.x + face.box.width / 2 - canvas.width / 2;
+          posX *= posY / canvas.height;
+          posX += canvas.width / 2;
+
+          ctx2.fillStyle = `rgba(0,255,0,${face.score})`;
+          ctx2.fillRect(c2.width - posX - 50, posY - 25, 100, 50);
           if (DEBUG)
             ctx2.strokeRect(
               face.box.x,
@@ -82,9 +85,10 @@ async function startup() {
               face.box.width,
               face.box.height
             );
-        };
-      });
-    };
-  }, 250);
+        });
+      };
+    },
+    HIGH_RATE ? 100 : 250
+  );
 }
 startup();
